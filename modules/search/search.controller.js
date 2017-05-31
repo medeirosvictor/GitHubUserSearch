109be5c477975a74042696c19e550be1fbb5b1ad
Victor Medeiros <victor.medeiros@ifnet-for.ifactory.com.br>
2017-05-31 09:25:56 -0300

    Improving layout of found users to grid sys.

--- modules/search/search.controller.js
+++ modules/search/search.controller.js
@@ -1,40 +1,34 @@
 angular.module('app').controller("searchController", userController);
 
-function userController($scope, $http, userFactory){   
-    $scope.user={}
-    $scope.searchUser = function() {
-        $scope.user = $scope.username;
-        if (!!$scope.location == false){ 
-            $http({
-                method: 'GET',
-                url: 'https://api.github.com/search/users?q='+$scope.username+'+in:fullname+repos:%3E4'
-            }).then( 
-                function success(response){
-                    console.log(response.data);
-                    $scope.userFound = response.data;
-            },
-                function error(){
-                    console.log('error on the user request!');
-                }
-            );
-        }else {
-            $http({
-                method: 'GET',
-                url: 'https://api.github.com/search/users?q='+$scope.username+'+in:fullname+repos:%3E4+location:'+$scope.location
-            }).then( 
-                function success(response){
-                    console.log(response.data);
-                    $scope.userFound = response.data;
-                    $scope.userFoundStatus = response.status;
-            },
-                function error(){
-                    console.log('error on the user request!');
-                }
-            );
-        }
-    }
-    $scope.setData = function (user){
+function userController($scope, $http, userFactory) {
+    $scope.user={};
+    
+    $scope.setData = function (user) {
         userFactory.setCurrentUser(user);
-        console.log(user);
     }
+    
+    $scope.searchUser = function() {
+
+        $scope.user = $scope.username;
+        var urlMain = 'https://api.github.com/search/users?q=';
+        var urlPartialLocation = '+in:fullname+repos:%3E4+location:';
+        
+        if (!!$scope.location == false){
+            var url = urlMain + $scope.username;
+        }else {
+            var url = urlMain + $scope.username + urlPartialLocation + $scope.location;
+        }            
+            $http({
+                method: 'GET',
+                url: url
+            }).then( 
+                function success(response){
+                    console.log(response.data);
+                    $scope.userFound = response.data;
+            },
+                function error(){
+                    console.log('error on the user request!');
+                }
+            );
+        }   
 }
